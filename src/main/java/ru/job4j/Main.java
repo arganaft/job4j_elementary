5615d385eda1a267b15aaca8e0343626d9ef5c5c
arganaft <e1mar.arganaft@gmail.com>
2022-12-07 20:46:37 +0300

    1.1.3. Тестирование / 0. Что такое тестирование

--- src/main/java/ru/job4j/Main.java
+++ src/main/java/ru/job4j/Main.java
@@ -1,6 +1,20 @@
 package ru.job4j;
+
+import ru.job4j.converter.Converter;
+
+import java.nio.charset.CharsetDecoder;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Привет мир!!!");
+        float in = 140;
+        float expected = 2;
+        float out = Converter.rubleToEuro(in);
+        boolean passed = expected == out;
+        System.out.println("140 rubles are 2. Test result : " + passed);
+        float inDollar = 120;
+        float expectedDollar = 2;
+        float outDollar = Converter.rubleToDollar(inDollar);
+        boolean passedDollar = expectedDollar == outDollar;
+        System.out.println("120 dollar are 2. Test result : " + passed);
     }
 }
