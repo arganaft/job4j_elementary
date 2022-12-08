e963873af8a6df4284d2a3fa0cf90095e5b20815
arganaft <e1mar.arganaft@gmail.com>
2022-12-08 23:42:20 +0300

    1.1.4. Оператор ветвления / 4.3.3. Логическое отрицание !

---
+++ src/main/java/ru/job4j/condition/LogicNot.java
@@ -0,0 +1,33 @@
+package ru.job4j.condition;
+
+public class LogicNot {
+
+    public static boolean isEven(int num) {
+        return num % 2 == 0;
+    }
+
+    public static boolean isPositive(int num) {
+        return num > 0;
+    }
+
+    public static boolean notEven(int num) {
+        return num % 2 != 0;
+    }
+
+    public static boolean notPositive(int num) {
+        return num < 0;
+    }
+
+    public static boolean notEvenAndPositive(int num) {
+        return num % 2 == 0 && num > 0;
+    }
+
+    public static boolean evenOrNotPositive(int num) {
+        return num % 2 == 0 || num > 0;
+    }
+
+    public  static void main(String[] args) {
+        System.out.println(LogicNot.isEven(0));
+    }
+
+}
