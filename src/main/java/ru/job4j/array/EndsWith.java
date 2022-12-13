9dbf2864cc775260dab2a17e2c639032c75b4b85
arganaft <e1mar.arganaft@gmail.com>
2022-12-13 18:13:19 +0300

    1.1.6. Массивы / 6.4.3. Слово заканчивается на ...

--- src/main/java/ru/job4j/array/EndsWith.java
+++ src/main/java/ru/job4j/array/EndsWith.java
@@ -3,11 +3,9 @@ package ru.job4j.array;
 public class EndsWith {
     public static boolean endsWith(char[] word, char[] post) {
         boolean result = true;
-        for (int i =  0; i < word.length; i++) {
+        for (int i =  0; i < post.length; i++) {
             if (word[word.length - 1 - i] != post[post.length - 1 - i]) {
                 result = false;
-                break;
-            } else if (i == post.length - 1) {
                 break;
             }
         }
