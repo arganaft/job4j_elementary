51c0bac62d1d5b8d7761ea2217d69609b95a4779
arganaft <e1mar.arganaft@gmail.com>
2022-12-13 18:22:25 +0300

    1.1.6. Массивы / 6.5.3. Сортировка выборкой.

--- src/test/java/ru/job4j/array/SortSelectedTest.java
+++ src/test/java/ru/job4j/array/SortSelectedTest.java
@@ -11,4 +11,28 @@ public class SortSelectedTest {
         int[] expected = new int[] {1, 2, 3, 4, 5};
         assertThat(result).containsExactly(expected);
     }
+
+    @Test
+    public void whenSort11And37And5And322And0() {
+        int[] data = new int[] {11, 37, 5, 322, 0};
+        int[] result = SortSelected.sort(data);
+        int[] expected = new int[] {0, 5, 11, 37, 322};
+        assertThat(result).containsExactly(expected);
+    }
+
+    @Test
+    public void whenSort5And9And5And7And3() {
+        int[] data = new int[] {5, 9, 5, 7, 3};
+        int[] result = SortSelected.sort(data);
+        int[] expected = new int[] {3, 5, 5, 7, 9};
+        assertThat(result).containsExactly(expected);
+    }
+
+    @Test
+    public void whenSort4And12() {
+        int[] data = new int[] {4, 12};
+        int[] result = SortSelected.sort(data);
+        int[] expected = new int[] {4, 12};
+        assertThat(result).containsExactly(expected);
+    }
 }
