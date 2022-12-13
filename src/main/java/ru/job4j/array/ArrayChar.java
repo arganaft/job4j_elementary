29dd9eec45122cb5d19061236eefbe266a9e31c4
arganaft <e1mar.arganaft@gmail.com>
2022-12-13 17:06:51 +0300

    1.1.6. Массивы / 6.4.0. Слово начинается с ...

--- src/main/java/ru/job4j/array/ArrayChar.java
+++ src/main/java/ru/job4j/array/ArrayChar.java
@@ -4,11 +4,9 @@ public class ArrayChar {
 
     public static boolean startsWith(char[] word, char[] pref) {
         boolean result = true;
-        for (int i = 0; i < word.length; i++) {
+        for (int i = 0; i < pref.length; i++) {
             if (word[i] != pref[i]) {
                 result = false;
-                break;
-            } else if (i == pref.length - 1) {
                 break;
             }
         }
